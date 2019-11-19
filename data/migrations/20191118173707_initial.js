11e275a0c63c7734efd1d5d20a5306278379e3c9
Wes Jonke <wesjonke@gmail.com>
2019-11-18 23:48:02 -0500

    successfully created base migration of database

--- data/migrations/20191118173707_initial.js
+++ data/migrations/20191118173707_initial.js
@@ -1,8 +1,102 @@
-
 exports.up = function(knex) {
-  
+  return (
+    knex.schema
+
+      //users table
+      .createTable("users", tbl => {
+        tbl.increments();
+        tbl.string("first_name", 255).notNullable();
+        tbl.string("last_name", 255).notNullable();
+        tbl
+          .string("username", 255)
+          .notNullable()
+          .unique();
+        tbl.string("password", 255).notNullable();
+        tbl.string("avatar", 255);
+      })
+
+      //parties table
+      .createTable("parties", tbl => {
+        tbl.increments();
+        tbl.string("name", 255).notNullable();
+        tbl.date("date");
+        tbl
+          .integer("budget")
+          .default(0)
+          .notNullable();
+      })
+
+      //items table
+      .createTable("items", tbl => {
+        tbl.increments();
+        tbl.string("name", 255).notNullable();
+        tbl.string("description", 500);
+        tbl
+          .integer("cost")
+          .default(0)
+          .notNullable();
+        tbl
+          .integer("user_id")
+          .unsigned()
+          .references("id")
+          .inTable("users")
+          .onDelete("RESTRICT")
+          .onUpdate("CASCADE")
+          .default(0)
+          .notNullable();
+        tbl
+          .integer("party_id")
+          .unsigned()
+          .references("id")
+          .inTable("parties")
+          .onDelete("RESTRICT")
+          .onUpdate("CASCADE")
+          .notNullable();
+      })
+
+      //images table
+      .createTable("images", tbl => {
+        tbl.increments();
+        tbl.string("img_url", 500).notNullable();
+        tbl
+          .integer("party_id")
+          .unsigned()
+          .references("id")
+          .inTable("parties")
+          .onDelete("RESTRICT")
+          .onUpdate("CASCADE")
+          .notNullable();
+      })
+
+      //user_party table
+      .createTable("user_party", tbl => {
+        tbl.increments();
+        tbl
+          .integer("user_id")
+          .unsigned()
+          .references("id")
+          .inTable("users")
+          .onDelete("RESTRICT")
+          .onUpdate("CASCADE")
+          .notNullable();
+        tbl
+          .integer("party_id")
+          .unsigned()
+          .references("id")
+          .inTable("parties")
+          .onDelete("RESTRICT")
+          .onUpdate("CASCADE")
+          .notNullable();
+      })
+  );
 };
 
 exports.down = function(knex) {
-  
+  //opposite of how they were created
+  return knex.schema
+    .dropTableIfExists("user_party")
+    .dropTableIfExists("images")
+    .dropTableIfExists("items")
+    .dropTableIfExists("parties")
+    .dropTableIfExists("users");
 };
