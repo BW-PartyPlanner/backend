24c16125330a66299a0d1c2914aa6ab97b87d252
Wes Jonke <wesjonke@gmail.com>
2019-11-19 14:14:21 -0500

    refactored seed files for syntax errors, seeds run and delete as normal now

--- data/seeds/04-images.js
+++ data/seeds/04-images.js
@@ -1,5 +1,6 @@
 exports.seed = function(knex) {
-  return knex("images")
-    .insert([{ img_url: "https://www.testurl.com", party_id: 1 }])
-    .insert([{ img_url: "https://www.testurltwo.com", party_id: 1 }]);
+  return knex("images").insert([
+    { img_url: "https://www.testurl.com", party_id: 1 },
+    { img_url: "https://www.testurltwo.com", party_id: 1 }
+  ]);
 };
