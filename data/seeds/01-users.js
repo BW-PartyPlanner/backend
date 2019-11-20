0ae956cdcedf59870471728b3c3daa656566a5b3
Wes Jonke <wesjonke@gmail.com>
2019-11-20 15:49:10 -0500

    added more robust seed data

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -1,24 +1,24 @@
 exports.seed = function(knex) {
   return knex("users").insert([
     {
-      first_name: "unassigned",
-      last_name: "unassigned",
-      username: "username",
+      first_name: "test",
+      last_name: "test",
+      username: "username1",
+      password: "password1",
+      avatar: "https://www.testurl.com"
+    },
+    {
+      first_name: "test2",
+      last_name: "test2",
+      username: "username2",
       password: "password",
       avatar: "https://www.testurl.com"
     },
     {
-      first_name: "John",
-      last_name: "Doe",
-      username: "Gunbunny37th",
-      password: "ilikechicken",
-      avatar: "https://www.testurl.com"
-    },
-    {
-      first_name: "jane",
-      last_name: "Doe",
-      username: "redleg134",
-      password: "idontknow",
+      first_name: "test3",
+      last_name: "test3",
+      username: "username3",
+      password: "password",
       avatar: "https://www.testurl.com"
     }
   ]);
--- data/seeds/04-images.js
+++ data/seeds/04-images.js
@@ -1,6 +1,14 @@
 exports.seed = function(knex) {
   return knex("images").insert([
     { img_url: "https://www.testurl.com", party_id: 1 },
-    { img_url: "https://www.testurltwo.com", party_id: 1 }
+    { img_url: "https://www.testurl.com", party_id: 1 },
+    { img_url: "https://www.testurl.com", party_id: 2 },
+    { img_url: "https://www.testurl.com", party_id: 3 },
+    { img_url: "https://www.testurl.com", party_id: 3 },
+    { img_url: "https://www.testurl.com", party_id: 3 },
+    { img_url: "https://www.testurl.com", party_id: 4 },
+    { img_url: "https://www.testurl.com", party_id: 5 },
+    { img_url: "https://www.testurl.com", party_id: 4 },
+    { img_url: "https://www.testurl.com", party_id: 2 }
   ]);
 };
