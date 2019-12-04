5fbec4121e0f74e35804bb1e00737895a99c4eb2
Wes Jonke <wesjonke@gmail.com>
2019-12-03 20:07:48 -0500

    changed status code on restricted middleware to a 401

--- data/seeds/05-user_party.js
+++ data/seeds/05-user_party.js
@@ -1,6 +1,6 @@
 exports.seed = function(knex) {
   return knex("user_party").insert([
-    { user_id: 1, party_id: 1, creator: true },
+    { user_id: 1, party_id: 1, creator:  },
     { user_id: 2, party_id: 2, creator: true },
     { user_id: 3, party_id: 3, creator: true },
     { user_id: 1, party_id: 4, creator: true },
